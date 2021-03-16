0112985566e2f6e9755c32f48e4689afc2564c63
fabio98p <parisifabio98@gmail.com>
2021-03-16 16:27:40 +0100

    studenti creazione di una tabella e inserimento di elementi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,31 @@
 console.log("ciao alessandro");
 
 $(document).ready(function () {
-	
-var student1 = {
-	nome: "alessandro",
-	cognome: "berone",
-	eta: 20,
-}
-console.log(student1);
-
+	var students = [
+		student1 = {
+			nome: "alessandro",
+			cognome: "berone",
+			eta: 20,
+		},
+		student2 = {
+			nome: "giorgiovanni",
+			cognome: "barlone",
+			eta: 40,
+		},
+		student3 = {
+			nome: "franceppippo",
+			cognome: "fumagalli",
+			eta: 29,
+		}
+	]
+	console.log(students);
+	//tento di inserire le key nella tabella
+	//tento di inserire le key nella tabella
+	for (let i = 0; i < students.length; i++) {
+		$("#students").append(`<tr id="student${i+1}"></tr>`);
+		for (const key in students[i]) {
+			$(`#student${i+1}`).append(`<td>${students[i][key]}</td>`);
+			//console.log(students[i][key]);
+		}
+	}
 })
