a5304db084d46cf001acd83f64544440a1b77fb8
fabio98p <parisifabio98@gmail.com>
2021-03-16 17:05:37 +0100

    studenti finito ma grezzo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 console.log("ciao alessandro");
 
 $(document).ready(function () {
+	//creazione di un array con all'interno alcuni studenti
 	var students = [
 		student1 = {
 			nome: "alessandro",
@@ -18,14 +19,25 @@ $(document).ready(function () {
 			eta: 29,
 		}
 	]
-	console.log(students);
-	//tento di inserire le key nella tabella
-	//tento di inserire le key nella tabella
-	for (let i = 0; i < students.length; i++) {
-		$("#students").append(`<tr id="student${i+1}"></tr>`);
-		for (const key in students[i]) {
-			$(`#student${i+1}`).append(`<td>${students[i][key]}</td>`);
-			//console.log(students[i][key]);
+	//aggiunta di un oggetto in un array
+	$("#btn").click(function () {
+		var newStudent = {
+			nome: $('#input-nome').val(),
+			cognome: $('#input-cognome').val(),
+			eta: $('#input-eta').val(),
 		}
-	}
+		students.push(newStudent)
+
+
+		//tento di inserire le key nella tabella
+		$("#students").html("")
+		for (let i = 0; i < students.length; i++) {
+			$("#students").append(`<tr id="student${i+1}"></tr>`);
+			for (const key in students[i]) {
+				$(`#student${i+1}`).append(`<td>${students[i][key]}</td>`);
+				//console.log(students[i][key]);
+			}
+		}
+	})
+
 })
